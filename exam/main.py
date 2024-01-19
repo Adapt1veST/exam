71cfa557e4af2031cdfa89b98a9fe83775671c4c
Adapt1veST <152629720+Adapt1veST@users.noreply.github.com>
2024-01-19 22:52:18 +0200

    Add files via upload

---
+++ exam/main.py
@@ -0,0 +1,74 @@
+class Auto: 
+    brand = "bmw" 
+    speed = 100   
+    def printer(self): 
+        self.speed = 100 + 50 
+        print(self.speed) 
+#xz = Auto() 
+#print(xz)         
+au = Auto() 
+au.printer() 
+ 
+#class Classicauto: 
+#    price = 100 
+#    def _speed(self): 
+#        self.splevel = 1 
+#        if (self.splevel == 1): 
+#            self.speed = 100 
+#        else : 
+#            self.speed = 200    
+#class Sportauto(Classicauto): 
+#    def speedl(self): 
+#        self.splevel = 2 
+#        if (self.splevel == 1): 
+#            self.speed = 100 
+#        else : 
+#            self.speed = 200             
+    
+#class Book: 
+#    name = "Harry poter" 
+#    author = "Jessica Rouling"  
+#    age = 1987 
+#    def printer(self): 
+#        print(self.name)  
+#        print(self.author) 
+#        print(self.age)  
+#bok = Book()  
+#bok.printer()  
+ 
+#class Fruits: 
+#    class Apple:       
+#        info = "Its green fruit" 
+#        def printer(self): 
+#            print(self.info)           
+#    class Banana: 
+#        inf = "Its yellow fruit" 
+#        def printerc(self): 
+#            print(self.inf)  
+#xzt = Fruits()   
+#zxc = Fruits()   
+#xzt.Apple.printer 
+#zxc.Banana.printerc 
+ 
+class Bank: 
+    numb = 1234567 
+    def _moneyminus(self): 
+          self.balance = 100 
+          self.balance - 50 
+          print(self.balance) 
+    def _moneyplus(self): 
+          self.balance = 100 
+          self.balance + 50 
+          print(self.balance)  
+ 
+user = Bank() 
+print(numb) 
+ 
+#class Monobank: 
+#    money = 50 
+#class Privatebank(First): 
+#    credit = 50  
+ 
+#x = (int(input())) 
+#y = x * x    
+#print(y)
